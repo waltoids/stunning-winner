fabd42a8ce6ff4d14dcc2c301011317f4bb3d98f
Walter Joy IV <waltoids@users.noreply.github.com>
2020-05-23 13:26:33 -0400

    keys, query and ajax

---
+++ script.js
@@ -0,0 +1,4 @@
+//initalizing and assigning authentication key
+let key = "mE0uKQNsUprie2i0qhGQRcFfjrhEXzZt";
+
+let queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + key + "&q=";
